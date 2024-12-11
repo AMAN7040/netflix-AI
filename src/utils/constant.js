ec4fd47a51881e4f6aac7d39c6b347baed0d3519
AMAN7040 <amansingh79620@gmail.com>
2024-12-11 15:18:48 +0530

    logo name updated

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -12,7 +12,7 @@ const HarmBlockThreshold = {
 };
 
 
-export const NETFLIX_LOGO = './Netflix_logo.png'
+export const NETFLIX_LOGO = './netflix_logo.png'
 
 export const BACKGROUND_IMAGE_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_large.jpg";
